80eb60d735a7366640b3e285f215d87edf574843
natasha jones <leo@natasha-joness-MacBook-Pro.local>
2014-07-02 20:50:47 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,7 +20,7 @@ describe "User Pages" do
  end
 
  describe "signup" do
-
+ 	
  	before { visit signup_path }
  	
  	let(:submit) { "Create my account" }
@@ -28,6 +28,13 @@ describe "User Pages" do
  	describe "with invalid information" do
  		it "should not create a user" do
  			expect { click_button submit }.not_to change(User, :count)
+ 		end
+
+ 	describe "after submission" do
+ 		before { click_button submit }
+
+ 		it { should have_title('Sign up') }
+ 		it { should have_content('error') }
  		end
  	end
 
@@ -42,6 +49,15 @@ describe "User Pages" do
  		it "should create a user" do
  			expect { click_button submit }.to change(User, :count).by(1)
  		end
- 	end
+ 	
+ 	describe "after saving the user" do
+ 		before { click_button submit }
+ 		let(:user) { User.find_by(email: 'user@example.com') }
+
+ 		it { should have_link('Sign out') }
+ 		it { should have_title(user.name) }
+ 		it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+  	end
  end
+end
 end
